a195790d168eb3ac01ed6ddc325e94a6a9136842
Joao Pedro <developerjotape@gmail.com>
2025-02-27 14:47:39 -0300

    ➕ build: dependências de animação e ícones adicionadas

--- tailwind.config.js
+++ tailwind.config.js
@@ -25,8 +25,8 @@ module.exports = {
       colors: {
         primary: "#1c1c22",
         accent: {
-          DEFAULT: "#00ff99",
-          hover: "#00e187",
+          DEFAULT: "#f43f5e",
+          hover: "#cc2d48",
         },
       },
 
