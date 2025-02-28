7d4b27c33ef7005fd1f87ab85c25fa66802c6271
Joao Pedro <developerjotape@gmail.com>
2025-02-28 09:34:48 -0300

    ✨ feat: navbar e header adicionados

---
+++ components/MobileNav.jsx
@@ -0,0 +1,70 @@
+"use client";
+
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { Menu } from "lucide-react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const links = [
+  {
+    name: "home",
+    path: "/",
+  },
+  {
+    name: "serviços",
+    path: "/services",
+  },
+  {
+    name: "sobre",
+    path: "/about",
+  },
+  {
+    name: "trabalhos",
+    path: "/work",
+  },
+  {
+    name: "contato",
+    path: "/contact",
+  },
+];
+
+const MobileNav = () => {
+  const pathname = usePathname();
+
+  return (
+    <Sheet>
+      <SheetTrigger className="flex justify-center items-center">
+        <Menu className="text-[32px] text-accent" />
+      </SheetTrigger>
+      <SheetContent className="flex flex-col">
+        {/* Logo */}
+        <div className="mt-32 mb-40 text-center text-2xl">
+          <Link href="/">
+            <h1 className="text-4xl font-semibold">
+              João Pedro<span className="text-accent">.</span>
+            </h1>
+          </Link>
+        </div>
+        {/* menu */}
+        <nav className="flex flex-col justify-center items-center gap-8">
+          {links.map((link, index) => {
+            return (
+              <Link
+                href={link.path}
+                key={index}
+                className={`${
+                  link.path === pathname &&
+                  "text-accent border-b-2 border-accent"
+                } text-xl capitalize hover:text-accent transition-all`}
+              >
+                {link.name}
+              </Link>
+            );
+          })}
+        </nav>
+      </SheetContent>
+    </Sheet>
+  );
+};
+
+export default MobileNav;
