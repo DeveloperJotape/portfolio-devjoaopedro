4cd76f1d351e39f92afb6fe2b7e0f660ad1ac015
Joao Pedro <developerjotape@gmail.com>
2025-03-05 14:53:38 -0300

    📝 feat: links adicionados

--- components/Social.jsx
+++ components/Social.jsx
@@ -2,9 +2,9 @@ import { Github, Instagram, Linkedin } from "lucide-react";
 import Link from "next/link";
 
 const socials = [
-  { icon: <Github />, path: "" },
-  { icon: <Linkedin />, path: "" },
-  { icon: <Instagram />, path: "" },
+  { icon: <Github />, path: "https://github.com/DeveloperJotape/" },
+  { icon: <Linkedin />, path: "https://www.linkedin.com/in/devjoaopedro/" },
+  { icon: <Instagram />, path: "https://www.instagram.com/jotape9411/" },
 ];
 
 const Social = ({ containerStyles, iconStyles }) => {
